fb870a289fd3a01fcf1179425dbe3063866671cb
Adam Clark <adam.clark@earthscope.org>
2026-02-17 10:04:30 -0800

    fix: safer staticfiles url lookup

--- markitup/util.py
+++ markitup/util.py
@@ -1,9 +1,17 @@
 from __future__ import unicode_literals
-
+from urllib.parse import urljoin
+from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
 
 
 def absolute_url(path):
-    if path.startswith(u'http://') or path.startswith(u'https://') or path.startswith(u'/'):
+    if (
+        path.startswith("http://")
+        or path.startswith("https://")
+        or path.startswith("/")
+    ):
         return path
-    return staticfiles_storage.url(path)
+    try:
+        return staticfiles_storage.url(path)
+    except ValueError:
+        return urljoin(settings.STATIC_URL, path)
